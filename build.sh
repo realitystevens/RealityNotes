f6cabb67c3e61414c58de30538c66b51414a525b
Stevens Reality <stevensreality@outlook.com>
2024-12-28 19:53:11 +0100

    Update & debug build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ python3 -m venv venv
 echo "Upgrading pip"
 pip install --upgrade pip
 
-echo "Install 'urllib' that requires OpenSSL 1.1.1+"
-pip install "urllib3<2.0"
-
 echo "Installing dependencies"
 pip install -r requirements.txt
+
+echo "Install 'urllib' that requires OpenSSL 1.1.1+"
+pip install "urllib3<2.0"
 
 echo "Collecting static files"
 python3 manage.py collectstatic --no-input
