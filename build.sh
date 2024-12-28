282ee8a6b6f96484ceb8f1dbe29e15472cdb798d
Reality Stevens <stevensreality@outlook.com>
2024-12-28 18:11:49 +0000

    Update file

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@
 # exit on error
 set -o errexit
 
+echo "Upgrading pip"
 pip install --upgrade pip
 
+echo "Installing dependencies"
 pip install -r requirements.txt
 
+echo "Collecting static files"
 python manage.py collectstatic --no-input
+
+echo "Deployment command from build.sh done"
