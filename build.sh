0d3708441ae72bbe6cc62091549adf15b188a705
Stevens Reality <stevensreality@outlook.com>
2024-12-28 20:39:34 +0100

    prod: commit for Render.com

--- build.sh
+++ build.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
 
 # exit on error
-set -ex
+set -o errexit
 
-echo "Create virtual environment"
-python3 -m venv venv
-
-echo "Activate virtual environment"
-source venv/bin/activate
-
-echo "Upgrading pip"
 pip install --upgrade pip
 
-echo "Installing dependencies"
 pip install -r requirements.txt
 
-echo "Install 'urllib' that requires OpenSSL 1.1.1+"
-pip install "urllib3<2.0"
-
-echo "Install 'gunicorn' for production"
-pip install 'gunicorn'
-
-echo "Collecting static files"
-python3 manage.py collectstatic --no-input
-
-echo "Deployment command from build.sh done"
+python manage.py collectstatic --no-input
