6a2d687af1cfe3f84914413ef280df25ffac53af
Stevens Reality <stevensreality@outlook.com>
2025-01-21 15:41:08 +0100

    refactor: refactored models.py and admin.py

--- app/admin.py
+++ app/admin.py
@@ -8,20 +8,13 @@ admin.site.site_title = 'Reality Notes'
 
 
 class ArticleAdmin(admin.ModelAdmin):
-
     list_display = ('title', 'url_hash', 'is_featured', 'published_at',)
-
     list_filter = ('published_at', 'is_featured',)
-
     search_fields = ('title__startswith', 'urlhash__startswith', 'published_at__startswith',)
-
 admin.site.register(Article, ArticleAdmin)
 
 
 class TagAdmin(admin.ModelAdmin):
-
     list_display = ('tag_name', 'tag_hash',)
-
     search_fields = ('tag_name__startswith', 'tag_hash__startswith',)
-
 admin.site.register(Tag, TagAdmin)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,5 @@
-import os
 import random
 from django.db import models
-from django.apps import apps
-from django.utils import timezone
 from django.utils.timezone import datetime
 from django.utils.html import strip_tags
 from cloudinary.models import CloudinaryField
@@ -16,38 +13,16 @@ def random_digits():
     return ''.join(random.choice('23456789') for i in range(5))
 
 def hash_generate():
-    hash = []
-    random_numbers = random.choice('23456789')
-
-    for x in range(3):
-        hash.append(random_numbers)
-
-    return ''.join(hash)
+    return ''.join(random.choice('23456789') for x in range(3))
 
 
 
 class Tag(models.Model):
-
-    tag_name = models.CharField(
-        default='',
-        max_length=50,
-        blank=False,
-        null=False,
-    )
-
-    tag_hash = models.CharField(
-        default=hash_generate,
-        max_length=5,
-        primary_key=True,
-        unique=True,
-        blank=False,
-        null=False,
-    )
-
+    tag_name = models.CharField(default='', max_length=50, blank=False, null=False,)
+    tag_hash = models.CharField(default=hash_generate, max_length=5, primary_key=True, unique=True, blank=False, null=False,)
 
     class Meta:
         ordering = ('tag_name', 'tag_hash',)
-
 
     def __str__(self):
         return self.tag_name
@@ -56,68 +31,15 @@ class Tag(models.Model):
 
 
 class Article(models.Model):
-
-    url_hash = models.SlugField(
-        max_length=5, 
-        default=random_digits,
-        primary_key=True,
-        unique=True,
-        help_text='Unique URL path for the Article',
-        blank=False,
-        null=False,
-    )
-
-    title = models.CharField(
-        max_length=1000, 
-        blank=False,
-        null=False,
-        default='', 
-        help_text='Title of the Article'
-    )
-
-    description = models.TextField(
-        max_length=1000, 
-        blank=False,
-        default=''
-    )
-
-    if config('ENV') == 'PROD':
-        image = CloudinaryField('image')
-    else:
-        image = models.ImageField(
-            upload_to='assets/article', 
-            blank=False, 
-            null=False, 
-            default=''
-            )
-
-    image_alt = models.CharField(
-        max_length=100,
-        blank=False,
-        null=False,
-        default=''
-    ) 
-
-    tag = models.ForeignKey(
-        Tag,
-        on_delete=models.SET_NULL,
-        null=True
-    )
-
+    url_hash = models.SlugField(max_length=5,  default=random_digits, primary_key=True, unique=True, help_text='Unique URL path for the Article', blank=False, null=False,)
+    title = models.CharField(max_length=1000, blank=False, null=False, default='', help_text='Title of the Article')
+    description = models.TextField(max_length=1000, blank=False, default='')
+    image = CloudinaryField('image') if config('ENV') == 'PROD' else models.ImageField(upload_to='assets/article', blank=False, null=False, default='')
+    image_alt = models.CharField(max_length=100, blank=False, null=False, default='') 
+    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
     content = RichTextUploadingField()
-
-    is_featured = models.BooleanField(
-        default=False,
-        help_text='Featured Article? (Will display as first Article on Homepage)'
-    )
-
-    published_at = models.DateTimeField(
-        default=datetime.now, 
-        editable=True, 
-        null=False, 
-        blank=False    
-    )
-
+    is_featured = models.BooleanField(default=False, help_text='Featured Article? (Will display as first Article on Homepage)')
+    published_at = models.DateTimeField(default=datetime.now, editable=True, null=False, blank=False)
 
     def mins_read(self):
         """
@@ -125,9 +47,8 @@ class Article(models.Model):
             split() splits the content into a list of words.
             len() returns the length of the list.
         """
-        num_of_words = len(strip_tags(self.content).split())
-
         words_per_minute = 150
+        num_of_words = len(strip_tags(self.content).split())
 
         mins_read = num_of_words / words_per_minute
 
@@ -137,9 +58,6 @@ class Article(models.Model):
             mins_read = f'{round(mins_read)} Mins Read'
 
         return mins_read
-
-
-
 
     class Meta:
         ordering = ('title', 'published_at', 'url_hash',)
